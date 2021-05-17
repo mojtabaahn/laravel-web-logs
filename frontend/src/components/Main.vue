d1ff5abada313d32aed94f5164c94b5f6b6c9ead
Mojtabaa.H.N <mojtaba.habibian@basalam.com>
2021-05-17 13:43:20 +0430

    fix: front

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -63,14 +63,14 @@
                     <div v-if="store.loading_log" class="w-full h-[50vh] flex items-center justify-center">
                         <Loading/>
                     </div>
-                    <div v-else-if="store.is_end" class="w-full h-[50vh] flex items-center justify-center">
-                        <div class="uppercase text-xl font-light text-gray-600">End Of File</div>
-                    </div>
-                    <div v-if="store.sorted_content.length === 0" class="w-full h-[50vh] flex items-center justify-center">
+                    <div v-else-if="store.sorted_content.length === 0" class="w-full h-[50vh] flex items-center justify-center">
                         <div class="text-center">
                             <div class="uppercase text-xl font-bold text-gray-600">Such An Empty</div>
                             <div class="text-gray-400 mt-2"> try removing filters</div>
                         </div>
+                    </div>
+                    <div v-else-if="store.is_end" class="w-full h-[50vh] flex items-center justify-center">
+                        <div class="uppercase text-xl font-light text-gray-600">End Of File</div>
                     </div>
                 </div>
             </div>
