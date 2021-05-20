46599f1ad8b9d146da12525958ec4c0390fd09d1
Christian QH <Oxicode@users.noreply.github.com>
2021-05-20 18:56:33 -0500

    custom path (#2)
    Co-authored-by: Oxicode <juan.quispe@upc.pe>

--- config/web-logs.php
+++ config/web-logs.php
@@ -1,6 +1,19 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Web Logs
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI path where Laravel Web Logs will be accessible from.
+    | Feel free to change this path to anything you like.
+    |
+    | Note that the URI will not affect the paths of its internal API that
+    | aren't exposed to users.
+    |
+    */
+    'path' => 'web-logs',
     'enabled' => env('APP_DEBUG', true),
     'lines_per_page' => env('WEB_LOGS_LINES_PER_PAGE', 1000)
 ];
